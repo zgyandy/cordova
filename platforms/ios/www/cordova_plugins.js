7256851758897a9be129eb29796572b0655f2f8f
zgy <gy.zhang@haitongwangxiao.com>
2019-12-12 14:35:23 +0800

    iOS图标修改

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -34,7 +34,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.4",
-    "cordova-plugin-camera": "4.1.0"
+    "cordova-plugin-camera": "4.1.0",
+    "cordova-plugin-whitelist": "1.3.4"
   };
 });
